2a89a81386f296054f3bbc0d8003a8285c81df19
Shota-Tabaghua1 <stabagua97@gmail.com>
2021-11-07 21:34:10 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,54 @@
+// Ex 1
+
+// let arr = [7, 8, 9, 'string', false];
+// console.log(Array.isArray(arr));
+
+// Ex 2 
+//    let arr5 = [1, 2, 3, 4, 5, 6, [7, 8, 9]];
+//    console.log(arr5.flat())
+
+// Ex 3
+
+// let arr = [7, 8, 9, 'string', false];
+// console.log(arr[arr.length - 1]);
+
+// Ex 4
+
+// let arr = [1, true, undefined, null];
+// arr.fill('string')
+// console.log(arr)
+
+// Ex 5
+
+// let s = 'Hello World'
+// const arr = s.split('e');
+// console.log(arr.join('*'))
+
+// Ex 6
+
+
+
+
+
+
+// Ex 7 
+
+// let arr = [ 4, 5, 6, 7];
+// const last = arr.pop();
+// console.log(arr)
+// let arr1 = [4, 5, 6]
+// arr1.unshift(7)
+// console.log (arr1)
+
+// Ex 8
+
+// let arr1 = [2, 3, 4];
+// let arr2 = [5, 6, 7];
+// let arr3 = [8, 9, 10];
+// const arr = arr1.concat(arr2, arr3);
+// console.log(arr);
+
+// Ex 9 
+
+// const s = String.fromCharCode(107)
+// console.log(s)
